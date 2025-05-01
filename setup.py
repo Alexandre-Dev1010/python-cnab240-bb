b12f9f4c7e113e8964adf1f4f13f5b616e894c19
xande <xande09010@gmail.com>
2025-05-01 18:33:07 -0300

    Corrige setup.py e MANIFEST.in para incluir arquivos JSON

--- setup.py
+++ setup.py
@@ -5,26 +5,19 @@ setup(
     version="0.1.0",
     packages=find_packages(),
     include_package_data=True,
-    package_data={
-        "cnab240.bancos.banco_brasil.specs": ["*.json"],
-        "cnab240.bancos.bradesco.specs": ["*.json"],
-        "cnab240.bancos.cecred.specs": ["*.json"],
-        "cnab240.bancos.cef.specs": ["*.json"],
-        "cnab240.bancos.itau.specs": ["*.json"],
-        "cnab240.bancos.santander.specs": ["*.json"],
-        "cnab240.bancos.sicoob.specs": ["*.json"],
-        "cnab240.bancos.sicredi.specs": ["*.json"],
-    },
+    package_data={"": ["*.json"]},
     install_requires=[
         "python-dateutil"
     ],
     author="Alexandre Ferreira",
     description="Geração de arquivos CNAB240 para o Banco do Brasil adaptado com campos válidos para boletos.",
-    long_description=open("README.md").read(),
+    long_description=open("README.md", encoding="utf-8").read(),
     long_description_content_type="text/markdown",
     url="https://github.com/Alexandre-Dev1010/python-cnab240-bb",
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent"
     ],
+    python_requires='>=3.6',
 )
